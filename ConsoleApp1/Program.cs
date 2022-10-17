027d18b851f36be3476c853aa5d5ed6ddb6df14c
SashaYaroshenko <yaroshenko2019ol@gmail.com>
2022-10-17 15:09:00 +0300

    Add VS project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Kata
+    {
+        public static bool Alphanumeric(string str)
+        {
+            bool flag = true;
+            foreach(char i in str)
+            {
+                if(Char.IsLetterOrDigit(i))
+                {
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+        }
+    }
+}
